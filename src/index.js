fa32ebe5bc52143c1be6e9cf75c678fda7a910a5
Felipe Rafael Goncalves <fgoncalves@cpqd.com>
2022-02-24 14:28:45 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ app.use(express.json());
 
 const customers=[];
 
+//Middleware
+function verifyIfExistsAccountCPF(req, res, next){
+    const {cpf} = req.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer){
+        return res.status(400).json({error: "Customer not found."})
+    }
+
+    req.customer= customer;
+
+    return next();
+}
+
 app.post('/account', (req, res) =>{
     const {cpf, name} = req.body;
 
@@ -25,16 +40,25 @@ app.post('/account', (req, res) =>{
     return res.status(201).send();
 });
 
-app.get('/statement', (req, res) =>{
-    const {cpf} = req.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer){
-        return res.status(400).json({error: "Customer not found."})
-    }
-
+app.get('/statement', verifyIfExistsAccountCPF, (req, res) =>{
+    const {customer} = req;
     return res.json(customer.statement);
 })
+
+
+app.post('/deposit', verifyIfExistsAccountCPF, (req, res) =>{
+    const {customer} = req;
+    const {description, amount} = req.body;
+    
+    const statementOperation = {
+        description,
+        amount,
+        date_at: new Date(),
+        type:'credit'
+    }
+
+    customer.statement.push(statementOperation);
+    return res.send(201).send();
+});
 
 app.listen(3333);
